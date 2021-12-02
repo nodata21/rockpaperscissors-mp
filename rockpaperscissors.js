ae4820ec1d5b8ddf301018429ada97a88409d97c
nodata21 <82617372+nodata21@users.noreply.github.com>
2021-12-02 02:08:14 +0100

    Working Alpha State

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,140 +1,9 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var input = document.getElementById("input");
 
-var express = require('express');
-app.use(express.static('public'));
-
-app.get('/', function(req, res){ res.sendFile('F:/node.js/rockpaperscissors/rockpaperscissors.html');
-});
-
-//var roomName;
-var roomData = {roomName : '', noOfPlayers : 0};
-var noOfPlayers = 0;
-var playerOne = {playerID : '', rn : '', 'set' : 0, playerName : '', hand : ''};
-var playerTwo = {playerID : '', rn : '', 'set' : 0, playerName : '', hand : ''};
-
-var board = {roomName: '', pOne : playerOne, pTwo : playerTwo}
-
-//Whenever someone connects this gets executed
-io.on('connection', function(socket){
-	console.log('A user connected');
-	socket.emit('welcome-screen', 'default');
-	socket.on('submitPlayerData', function(data){
-	   console.log(board.pOne.playerName.length);
-	   //var roomData = {roomName : '', noOfPlayers : 0};
-	   //console.log(user.userID);
-	   console.log(board);
-	   //prepare room
-	   roomData.roomName = data.rn;
-	   //console.log(roomData.noOfPlayers);
-	   //go on with this part
-	   if (!io.sockets.adapter.rooms.has(roomData.roomName)) {
-			socket.join(roomData.roomName);
-			console.log(io.sockets.adapter.rooms.get(roomData.roomName).size);
-			if (board.pOne.playerName.length < 1) {
-				board.pOne.playerID = socket.id;
-				board.pOne.rn = roomData.roomName;
-				board.pOne.playerName = data.pn;
-				
-				//socket.join(roomData.roomName);
-				console.log('one');
-			}
-			console.log(board.pOne.playerName); console.log(board.pTwo.playerName);
-			//console.log(roomData.noOfPlayers);
-			//setup board
-			socket.emit('setBoard', data);
-			console.log(data);
-			//code to 'slowly' start the game
-			socket.on('playedHand', function(data) {
-				console.log(data);
-				if (board.pOne.playerName == data.pn) { board.pOne.set = 1; board.pOne.hand = data.hand;}
-				console.log(data);
-				if (board.pTwo.playerName == data.pn) { board.pTwo.set = 1; board.pTwo.hand = data.hand;}
-				console.log(board);
-				if (board.pOne.set == 1 && board.pTwo.set == 1) {
-					io.sockets.in(roomData.roomName).emit('setHand', {playerOneHand : board.pOne.hand, playerTwoHand : board.pTwo.hand});
-					console.log(board.pOne.set); console.log(board.pTwo.set); console.log(roomData.roomName);
-					board.pOne.set = 0; board.pTwo.set = 0;
-				}
-			});
-		}
-		
-		else if (io.sockets.adapter.rooms.has(roomData.roomName)) {
-			console.log(io.sockets.adapter.rooms.get(roomData.roomName).size);
-			if (io.sockets.adapter.rooms.get(roomData.roomName).size < 2 /*roomData.noOfPlayers < 2*/) {
-				if (board.pOne.playerName.length < 1) {
-					board.pOne.playerID = socket.id;
-					board.pOne.rn = roomData.roomName;
-					board.pOne.playerName = data.pn;
-					socket.join(roomData.roomName);
-					console.log('one');
-				}
-				else if (board.pTwo.playerName.length < 1){
-					board.pTwo.playerID = socket.id;
-					board.pTwo.rn = roomData.roomName;
-					board.pTwo.playerName = data.pn;
-					socket.join(roomData.roomName);
-					console.log('two');
-				}
-			console.log(io.sockets.adapter.rooms.get(roomData.roomName).size);
-				console.log(board.pOne.playerName); console.log(board.pTwo.playerName);
-				console.log(roomData.noOfPlayers);
-				//setup board
-				socket.emit('setBoard', data);
-				console.log(data);
-				//code to 'slowly' start the game
-				socket.on('playedHand', function(data) {
-					console.log(data);
-					if (board.pOne.playerName == data.pn) { board.pOne.set = 1; board.pOne.hand = data.hand;}
-					console.log(data);
-					if (board.pTwo.playerName == data.pn) { board.pTwo.set = 1; board.pTwo.hand = data.hand;}
-					console.log(board);
-					if (board.pOne.set == 1 && board.pTwo.set == 1) {
-						io.sockets.in(roomData.roomName).emit('setHand', {playerOneHand : board.pOne.hand, playerTwoHand : board.pTwo.hand});
-						console.log(board.pOne.set); console.log(board.pTwo.set); console.log(roomData.roomName);
-						board.pOne.set = 0; board.pTwo.set = 0;
-					}
-				});
-			}
-			else {
-				//socket.leave(roomData.roomName);
-				console.log('Room is full!');   
-			}
-			
-		}
-	socket.on('disconnecting', function () {
-		
-		console.log(socket.rooms);
-		
-		if (board.pOne.playerID == socket.id) {
-			board.pOne.playerID = '';
-			board.pOne.playerName = '';
-		}
-		if (board.pTwo.playerID == socket.id) {
-			board.pTwo.playerID = '';
-			board.pTwo.playerName = '';
-		}
-		board.pOne.set = 0; board.pTwo.set = 0;
-   });
-   })
-   //Whenever someone disconnects this piece of code executed
-   socket.on('disconnect', function () {
-	  console.log('A user disconnected');
-	  console.log(socket.rooms);
-   });
-});
-
-http.listen(3000, function(){
-   console.log('listening on *:3000');
-});
-
-
-
-
-
-/*
-var handplayer;
+var rock = document.getElementById("rock");
+var paper = document.getElementById("paper");
+var scissors = document.getElementById("scissors");
+var well = document.getElementById("well");
 
 var output = document.getElementById("output"); var first = document.getElementById("first"); var put = document.getElementById("last");
 
@@ -146,6 +15,7 @@ const hand = ['rock', 'paper', 'scissors', 'well'];
 
 function play() {
     
+    cpu = Math.floor(Math.random() * 4);
     input.value = text = this.id;
     
     if ( beats[text].includes(hand[cpu]) ) {
@@ -170,5 +40,3 @@ rock.addEventListener("click", play)
 paper.addEventListener("click", play)
 scissors.addEventListener("click", play)
 well.addEventListener("click", play)
-
-*/
